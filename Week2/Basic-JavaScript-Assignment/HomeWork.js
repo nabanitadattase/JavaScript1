1f464c984e40eaebd1ed670d2e7f5e9631d547a9
nabanitadattase <47370928+nabanitadattase@users.noreply.github.com>
2019-08-30 02:39:17 +0200

    Basic JavaScript Assignment

---
+++ Week2/Basic-JavaScript-Assignment/HomeWork.js
@@ -0,0 +1,72 @@
+console.log("Hello World!");//To print Hello World
+console.log('I m awesome');
+let x;
+console.log("the value of my variable x will be: 05091986");
+console.log(x);
+x = 05091986;
+console.log("The value of x is: 05091986");
+console.log(x);
+let y = "Nabanita";
+console.log("The Value of y is: Nabanita");
+console.log(y);
+ y = "Hello Guys";
+console.log("The value of y is: Hello Guys");
+console.log(y);
+let z= 7.25;
+console.log(z);
+let a= 7;
+console.log(a);
+let b;
+if (z>a)
+b=z;
+else
+b=a;
+console.log(b);
+//Array
+let arr =[];
+console.log("The value of array is: 5");
+console.log(arr);
+let arr1=["Cat","Cow","Dog","Goat"];
+console.log(arr1);
+let Daan_favorite_animal ="baby pig";
+console.log(arr1 + "," + Daan_favorite_animal);
+let myString = "this is a test";
+console.log(myString);
+let c=myString.length;
+console.log(c);
+let e= "Hey";
+console.log(e);
+console.log("The type of the variable is: String");
+console.log(typeof(e));
+let f=20;
+console.log(f);
+console.log("The type of the variable is: Integer");
+console.log(typeof(f));
+if(typeof(e)===typeof(f))
+console.log("They are of same type");
+else
+console.log("They are of different type");
+let g=true;
+console.log(g);
+console.log("The type of the variable is: Boolean");
+console.log(typeof(g));
+let h='S';
+console.log(h);
+console.log("The type of the variable is: Character");
+console.log(typeof(h));
+if(typeof(g)===typeof(h))
+console.log("They are of same type");
+else
+console.log("They are of different type");
+//How remainder works
+let d=7;
+d= d%3;
+console.log(d+3);
+//Different data types in an array
+arr2 =["Nabanita",3];
+console.log(arr2);
+//Compare Infinity
+if(6/0===10/0)
+console.log("Infinity value can be compare");
+else
+console.log("Infinity value can not be compare");
